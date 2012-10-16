6f24aea3970097366d1f3f2677c29babc267433f
Daniel James <dwhjames@gmail.com>
2012-10-16 14:43:22 -0400

    further optimize insertion and balancing of RedBlackSet
    further split balancing function to the level of grandchildren (Exercise 3.10b)
    specialize insertion to inserting on the left and on the right (cf. http://github.com/imsut/pfds)
    add convenience copying method to Branch (cf. http://www.jayconrod.com/posts/32/)

--- src/main/scala/chapter3/RedBlackSet.scala
+++ src/main/scala/chapter3/RedBlackSet.scala
@@ -56,6 +56,7 @@ sealed abstract class RedBlackSet[+A <% Ordered[A]] {
   }
 */
 
+/* insert with split balancing smart constructors (lbalance, rbalance)
   def insert[B >: A <% Ordered[B]](x: B): RedBlackSet[B] = {
     def ins(set: RedBlackSet[A]): Branch[B] = set match {
       case Leaf => Branch(Red, Leaf, x, Leaf)
@@ -68,6 +69,42 @@ sealed abstract class RedBlackSet[+A <% Ordered[A]] {
     }
     val Branch(_, a, y, b) = ins(this)
     Branch(Black, a, y, b)
+  }
+*/
+
+  def insert[B >: A <% Ordered[B]](x: B): RedBlackSet[B] = {
+    def insl(branch: Branch[A]): Branch[B] = branch.left match {
+      case Leaf =>
+        branch.copyWith(left = Branch(Red, Leaf, x, Leaf))
+      case l @ Branch(_, _, y, _) =>
+        if (x < y)
+          llbalance(branch.color, insl(l), branch.elem, branch.right)
+        else if (x > y)
+          lrbalance(branch.color, insr(l), branch.elem, branch.right)
+        else
+          branch
+    }
+    def insr(branch: Branch[A]): Branch[B] = branch.right match {
+      case Leaf =>
+        branch.copyWith(right = Branch(Red, Leaf, x, Leaf))
+      case r @ Branch(_, _, y, _) =>
+        if (x < y)
+          rlbalance(branch.color, branch.left, branch.elem, insl(r))
+        else if (x > y)
+          rrbalance(branch.color, branch.left, branch.elem, insr(r))
+        else
+          branch
+    }
+    this match {
+      case Leaf => Branch(Black, Leaf, x, Leaf)
+      case b @ Branch(_, _, y, _) => {
+        if (x < y)
+          insl(b).copyWith(color = Black)
+        else if (x > y)
+          insr(b).copyWith(color = Black)
+        else this
+      }
+    }
   }
 }
 
@@ -103,6 +140,7 @@ object RedBlackSet {
   }
 */
 
+/* left balancing for Exercise 3.10(a)
   private[chapter3] def lbalance[A <% Ordered[A]](
     color: Color,
     left: RedBlackSet[A],
@@ -117,7 +155,29 @@ object RedBlackSet {
         case _ => Branch(color, left, elem, right)
       }
   }
+*/
 
+  private[chapter3] def llbalance[A <% Ordered[A]](
+    color: Color,
+    left:  RedBlackSet[A],
+    elem:  A,
+    right: RedBlackSet[A]): Branch[A] = (color, left, elem, right) match {
+      case (Black, Branch(Red, Branch(Red, a, x, b), y, c), z, d) =>
+        Branch(Red, Branch(Black, a, x, b), y, Branch(Black, c, z, d))
+      case _ => Branch(color, left, elem, right)
+  }
+
+  private[chapter3] def lrbalance[A <% Ordered[A]](
+    color: Color,
+    left:  RedBlackSet[A],
+    elem:  A,
+    right: RedBlackSet[A]): Branch[A] = (color, left, elem, right) match {
+      case (Black, Branch(Red, a, x, Branch(Red, b, y, c)), z, d) =>
+        Branch(Red, Branch(Black, a, x, b), y, Branch(Black, c, z, d))
+      case _ => Branch(color, left, elem, right)
+  }
+
+/* right balancing for Exercise 3.10(a)
   private[chapter3] def rbalance[A <% Ordered[A]](
     color: Color,
     left: RedBlackSet[A],
@@ -132,6 +192,27 @@ object RedBlackSet {
         case _ => Branch(color, left, elem, right)
       }
   }
+*/
+
+  private[chapter3] def rlbalance[A <% Ordered[A]](
+    color: Color,
+    left:  RedBlackSet[A],
+    elem:  A,
+    right: RedBlackSet[A]): Branch[A] = (color, left, elem, right) match {
+      case (Black, a, x, Branch(Red, Branch(Red, b, y, c), z, d)) =>
+        Branch(Red, Branch(Black, a, x, b), y, Branch(Black, c, z, d))
+      case _ => Branch(color, left, elem, right)
+  }
+
+  private[chapter3] def rrbalance[A <% Ordered[A]](
+    color: Color,
+    left:  RedBlackSet[A],
+    elem:  A,
+    right: RedBlackSet[A]): Branch[A] = (color, left, elem, right) match {
+      case (Black, a, x, Branch(Red, b, y, Branch(Red, c, z, d))) =>
+        Branch(Red, Branch(Black, a, x, b), y, Branch(Black, c, z, d))
+      case _ => Branch(color, left, elem, right)
+  }
 
   sealed abstract class Color
   case object Red extends Color
@@ -142,10 +223,18 @@ object RedBlackSet {
   }
 
   final case class Branch[+A <% Ordered[A]](
-      private val color: Color,
-      private val left: RedBlackSet[A],
-      private val elem: A,
-      private val right: RedBlackSet[A]) extends RedBlackSet[A] {
+      val color: Color,
+      val left:  RedBlackSet[A],
+      val elem:  A,
+      val right: RedBlackSet[A]) extends RedBlackSet[A] {
     override def isEmpty = false
+
+    def copyWith[B >: A <% Ordered[B]]
+                (color: Color          = color,
+                 left:  RedBlackSet[B] = left,
+                 elem:  B              = elem,
+                 right: RedBlackSet[B] = right): Branch[B] = {
+      Branch(color, left, elem, right)
+    }
   }
 }
